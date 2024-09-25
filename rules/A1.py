d63b12d08a03d696861c244afe39d5812ad128dd
LamaHamid <abdulhamid_13@hotmail.com>
2024-09-25 14:53:10 +0200

    Update A1.py

--- rules/A1.py
+++ rules/A1.py
@@ -1,23 +1,29 @@
 # BIManalyst Group 01
 import ifcopenshell
+from bonsai.bim.ifc import IfcStore
+file = IfcStore.get_file()
 
-def checkRule(model):
+import bpy
 
-    floors = model.by_type('IfcBuildingStorey')
+def checkRule(floors):
     
-    floors.sort(key=lambda obj: obj.ObjectPlacement.RelativePlacement.Location.Coordinates[2])
-
+    floors.sort(key=lambda obj: obj.location.z)
+    
     for i in range(1, len(floors)):
         floor1 = floors[i-1]
         floor2 = floors[i]
         
-        z1 = floor1.ObjectPlacement.RelativePlacement.Location.Coordinates[2]
-        z2 = floor2.ObjectPlacement.RelativePlacement.Location.Coordinates[2]
+        z1 = floor1.location.z
+        z2 = floor2.location.z
         
         elevation = z2 - z1
-        print(f"Elevation between {floor1.name} and {floor2.name}: {elevation} Blender units")
-        
-#things = file.by_type('IfcBuildingStorey')
-#print("num of elevations", len(things))
+        print(f"Elevation between {floor1.name} og {floor2.name}: {elevation} Blender units")
+
+floors = [obj for obj in bpy.data.objects if "IfcBuildingStorey" in obj.name]
+
+checkRule(floors)
+
+things = file.by_type('IfcBuildingStorey')
+print("num of elevations in IFC-model:", len(things))
         
 
