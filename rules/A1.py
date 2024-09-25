752ed6e75847564018249230a63e378b953edc9e
Hajarb <s184359@student.dtu.dk>
2024-09-25 10:26:17 +0200

    Update A1.py

--- rules/A1.py
+++ rules/A1.py
@@ -1,36 +1,22 @@
+# BIManalyst Group 01
 import ifcopenshell
-from bonsai.bim.ifc import IfcStore
-file = IfcStore.get_file()
 
-import bpy
+def checkRule(model):
 
-def calculate_floor_elevations():
-    # Get the IFC file (you may want to handle the case where the file is not found)
-    file = IfcStore.get_file()
+    floors = model.by_type('IfcBuildingStorey')
+    
+    floors.sort(key=lambda obj: obj.ObjectPlacement.RelativePlacement.Location.Coordinates[2])
 
-    # Find all objects that are part of the building storey
-    floors = [obj for obj in bpy.data.objects if "IfcBuildingStorey" in obj.name]
-
-    # Sort floors by their Z-coordinate (height)
-    floors.sort(key=lambda obj: obj.location.z)
-
-    # Calculate elevations between floors
     for i in range(1, len(floors)):
         floor1 = floors[i-1]
         floor2 = floors[i]
         
-        z1 = floor1.location.z
-        z2 = floor2.location.z
+        z1 = floor1.ObjectPlacement.RelativePlacement.Location.Coordinates[2]
+        z2 = floor2.ObjectPlacement.RelativePlacement.Location.Coordinates[2]
         
         elevation = z2 - z1
         print(f"Elevation between {floor1.name} and {floor2.name}: {elevation} Blender units")
         
-        things = file.by_type('IfcBuildingStorey')
-        print("num of elevations", len(things))
-# Call the function
-calculate_floor_elevations()
 
+        
 
-
-things = file.by_type('IfcBuildingStorey')
-print("num of elevations", len(things))
