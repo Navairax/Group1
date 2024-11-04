07c9d485b98de61317652642ba02f478c7e4a68d
Navairax <s214364@dtu.dk>
2024-11-04 14:55:29 +0100

    Update main.py

--- A3/main.py
+++ A3/main.py
@@ -1,1 +1,9 @@
 
+from pathlib import Path
+import ifcopenshell
+model_path = Path("C:/Users/navai/OneDrive/Skrivebord/3. semester/41934 Vidergående BIM/CES_BLD_24_10_ARC.ifc")
+
+if not model_path.is_file():
+    raise FileNotFoundError(f"No file found at {model_path}!")
+
+model = ifcopenshell.open(model_path)
